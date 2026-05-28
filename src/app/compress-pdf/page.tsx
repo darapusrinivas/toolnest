14dec62fd51e0f12faa4efd27729cb9d22f2ac2d
Srinivas <darapusri@gmail.com>
2026-05-28 15:21:36 +0530

    Fixed compress pdf build error

--- src/app/compress-pdf/page.tsx
+++ src/app/compress-pdf/page.tsx
@@ -24,7 +24,7 @@ export default function CompressPdfPage() {
       useObjectStreams: true,
     });
 
-    const blob = new Blob([compressedBytes], {
+    const blob = new Blob([compressedBytes.buffer], {
       type: "application/pdf",
     });
 
